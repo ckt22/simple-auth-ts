e727fcd4be709f9ebf2a92f65a32e80a69933476
kit.tang <kit.tang@atomintl.com>
2022-12-26 12:56:11 +0800

    heroku cleardb - allow port to be undefined

--- src/database/index.ts
+++ src/database/index.ts
@@ -7,7 +7,7 @@ import 'dotenv/config';
 export const AppDataSource = new DataSource({
     type: 'mysql',
     host: process.env.MYSQL_HOST,
-    port: Number(process.env.MYSQL_PORT),
+    port: Number(process.env.MYSQL_PORT) || undefined,
     username: process.env.MYSQL_USERNAME,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
